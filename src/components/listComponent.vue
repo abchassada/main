f3c8b5cabd6b34de74503f2c43a451021c3967fa
buxinran <141600249+buxinran@users.noreply.github.com>
2024-03-06 15:13:40 +0800

    ab

--- src/components/listComponent.vue
+++ src/components/listComponent.vue
@@ -63,18 +63,6 @@ const optionsGpu = [
     value: 'Option2',
     label: 'Option2',
   },
-  {
-    value: 'Option3',
-    label: 'Option3',
-  },
-  {
-    value: 'Option4',
-    label: 'Option4',
-  },
-  {
-    value: 'Option5',
-    label: 'Option5',
-  },
 ]
 const optionsPod = [
   {
