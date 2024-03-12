162f7caddaca6d56e7182245f64a8f2e38043846
sandwich-biscuit <22371415@buaa.edu.cn>
2024-03-12 20:42:25 +0800

    node改为host

--- src/components/projectTable.vue
+++ src/components/projectTable.vue
@@ -1,7 +1,7 @@
 <template>
   <el-table :data="tableData" border style="width: 100%" height="250">
     <el-table-column prop="pod" label="pod" width="342" />
-    <el-table-column prop="node" label="node" width="166" />
+    <el-table-column prop="host" label="host" width="166" />
     <el-table-column prop="ip" label="IP" width="166" />
     <el-table-column prop="gpu" label="GPUs" width="166" />
     <el-table-column prop="uuid" label="GPU UUIDs" width="343" />
@@ -13,42 +13,42 @@ import {ref} from 'vue'
 const tableData = ref([
     {
         pod: 'best-bert-yaml-with-data-set-1 a...',
-        node: 'dell-64',
+        host: 'dell-64',
         ip: '192.168.5.64',
         gpu: '0',
         uuid: 'GPU-dd41b996-e5a4-0795-b04...',
     },
     {
         pod: 'best-bert-yaml-with-data-set-1 a...',
-        node: 'dell-64',
+        host: 'dell-64',
         ip: '192.168.5.64',
         gpu: '1',
         uuid: 'GPU-dd41b996-e5a4-0795-b04...',
     },
     {
         pod: 'best-bert-yaml-with-data-set-1 a...',
-        node: 'dell-64',
+        host: 'dell-64',
         ip: '192.168.5.64',
         gpu: '0',
         uuid: 'GPU-dd41b996-e5a4-0795-b04...',
     },
     {
         pod: 'best-bert-yaml-with-data-set-1 a...',
-        node: 'dell-64',
+        host: 'dell-64',
         ip: '192.168.5.64',
         gpu: '1',
         uuid: 'GPU-dd41b996-e5a4-0795-b04...',
     },
     {
         pod: 'best-bert-yaml-with-data-set-1 a...',
-        node: 'dell-64',
+        host: 'dell-64',
         ip: '192.168.5.64',
         gpu: '0',
         uuid: 'GPU-dd41b996-e5a4-0795-b04...',
     },
     {
         pod: 'best-bert-yaml-with-data-set-1 a...',
-        node: 'dell-64',
+        host: 'dell-64',
         ip: '192.168.5.64',
         gpu: '1',
         uuid: 'GPU-dd41b996-e5a4-0795-b04...',
