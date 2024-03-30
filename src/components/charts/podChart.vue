5c01b941cf0cde71934237ad05ff5402ac43c450
qiuerOwO <108218606+maotougu@users.noreply.github.com>
2024-03-30 17:41:33 +0800

    放入图

--- src/components/charts/podChart.vue
+++ src/components/charts/podChart.vue
@@ -1,57 +1,133 @@
 <template>
-    <div ref="echartsContainer" style="height: 500px;"></div>
-</template>
+   <div ref="echartsContainer" style="height: 500px;"></div></template>
+<script>
+import {ref, onMounted} from 'vue'
+import * as echarts from 'echarts'
+export default {
+    setup() {
+        const echartsContainer = ref(null);
+       onMounted(()=>{
+            const myChart = echarts.init(echartsContainer.value);
+            var option;
 
-<script setup>
-import { onMounted, ref } from 'vue';
-import * as echarts from 'echarts';
-
-const echartsContainer = ref(null);
-
-onMounted(() => {
-    const myChart = echarts.init(echartsContainer.value);
-
-    // 这里假设了一些数据结构，具体情况需要根据实际数据进行调整
-    const layers = ['嵌入层', '特征交互层', 'MLP', '输出层']; // 神经网络的层
-    const resourceTypes = ['PCIe带宽', 'GPU计算资源', '以太网带宽']; // 资源类型
-
-    // 构建series数据，每种资源类型一个series
-    const series = resourceTypes.map(type => ({
-        name: type,
-        type: 'bar',
-        stack: '总量', // 此处假设资源使用可以堆叠
-        data: layers.map(() => Math.random() * 100) // 随机数据，实际应从数据源获取
-    }));
-
-    const option = {
-        title: {
-            text: '训练过程资源占用', // 标题
-        },
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'shadow'
+            var data = [];
+            var dataCount = 10;
+            var startTime = +new Date();
+            var categories = ['categoryA', 'categoryB', 'categoryC'];
+            var types = [
+            { name: 'JS Heap', color: '#7b9ce1' },
+            { name: 'Documents', color: '#bd6d6c' },
+            { name: 'Nodes', color: '#75d874' },
+            { name: 'Listeners', color: '#e0bc78' },
+            { name: 'GPU Memory', color: '#dc77dc' },
+            { name: 'GPU', color: '#72b362' }
+            ];
+            // Generate mock data
+            categories.forEach(function (category, index) {
+            var baseTime = startTime;
+            for (var i = 0; i < dataCount; i++) {
+                var typeItem = types[Math.round(Math.random() * (types.length - 1))];
+                var duration = Math.round(Math.random() * 10000);
+                data.push({
+                name: typeItem.name,
+                value: [index, baseTime, (baseTime += duration), duration],
+                itemStyle: {
+                    normal: {
+                    color: typeItem.color
+                    }
+                }
+                });
+                baseTime += Math.round(Math.random() * 2000);
             }
-        },
-        legend: {
-            data: resourceTypes
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: {
-            type: 'value'
-        },
-        yAxis: {
-            type: 'category',
-            data: layers
-        },
-        series: series
-    };
+            });
+            function renderItem(params, api) {
+            var categoryIndex = api.value(0);
+            var start = api.coord([api.value(1), categoryIndex]);
+            var end = api.coord([api.value(2), categoryIndex]);
+            var height = api.size([0, 1])[1] * 0.6;
+            var rectShape = echarts.graphic.clipRectByRect(
+                {
+                x: start[0],
+                y: start[1] - height / 2,
+                width: end[0] - start[0],
+                height: height
+                },
+                {
+                x: params.coordSys.x,
+                y: params.coordSys.y,
+                width: params.coordSys.width,
+                height: params.coordSys.height
+                }
+            );
+            return (
+                rectShape && {
+                type: 'rect',
+                transition: ['shape'],
+                shape: rectShape,
+                style: api.style()
+                }
+            );
+            }
+            option = {
+            tooltip: {
+                formatter: function (params) {
+                return params.marker + params.name + ': ' + params.value[3] + ' ms';
+                }
+            },
+            title: {
+                text: 'Profile',
+                left: 'center'
+            },
+            dataZoom: [
+                {
+                type: 'slider',
+                filterMode: 'weakFilter',
+                showDataShadow: false,
+                top: 400,
+                labelFormatter: ''
+                },
+                {
+                type: 'inside',
+                filterMode: 'weakFilter'
+                }
+            ],
+            grid: {
+                height: 300
+            },
+            xAxis: {
+                min: startTime,
+                scale: true,
+                axisLabel: {
+                formatter: function (val) {
+                    return Math.max(0, val - startTime) + ' ms';
+                }
+                }
+            },
+            yAxis: {
+                data: categories
+            },
+            series: [
+                {
+                type: 'custom',
+                renderItem: renderItem,
+                itemStyle: {
+                    opacity: 0.8
+                },
+                encode: {
+                    x: [1, 2],
+                    y: 0
+                },
+                data: data
+                }
+            ]
+            };
 
-    myChart.setOption(option);
-});
+            option && myChart.setOption(option);
+       })
+       
+        return {
+            echartsContainer
+        }
+    }
+}
 </script>
